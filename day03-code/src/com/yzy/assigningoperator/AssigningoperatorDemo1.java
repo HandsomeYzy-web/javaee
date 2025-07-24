306509354875e13e8676803bcc12848ff5c70733
HandsomeYzy <487542196@qq.com>
2025-07-24 10:37:22 +0800

    javaee

---
+++ day03-code/src/com/yzy/assigningoperator/AssigningoperatorDemo1.java
@@ -0,0 +1,15 @@
+package com.yzy.assigningoperator;
+
+public class AssigningoperatorDemo1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        a += b;
+        System.out.println(a);
+        System.out.println(b);
+        short s = 1;
+        //s =(short) (s + 1);
+        s += 1;
+        System.out.println(s);
+    }
+}
---
+++ day03-code/src/com/yzy/compareoperator/CompareoperatorDemo1.java
@@ -0,0 +1,11 @@
+package com.yzy.compareoperator;
+
+public class CompareoperatorDemo1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int c = a + b;
+        System.out.println(a == b);
+        System.out.println(a == c);
+    }
+}
